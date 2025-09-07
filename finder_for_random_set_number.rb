6d8418097cb9a5bdaa7c10cdbe0e81da60cba827
kaivongcom <234134+kaivongcom@users.noreply.github.com>
2025-09-07 19:10:26 +0100

    edit for indentation

--- finder_for_random_set_number.rb
+++ finder_for_random_set_number.rb
@@ -5,20 +5,20 @@ NUMBER_SET_AT = 21
 LN_BREAK = "\n"
 
 def print_calculations(numbers)
-	print "%s + %s" %numbers
+  print "%s + %s" %numbers
   print LN_BREAK + "ok!"  + LN_BREAK
 end
 
 def find_number(random_number=RNDM_NO)
-	NUMBER_SET_AT.times do |integer_1|
-		sum = {}
- 		  NUMBER_SET_AT.times do |integer_2|
-			these_numbers = [integer_1, integer_2]
-			sum[:calculated] = these_numbers[0] + these_numbers[1]
-	  	if (sum[:calculated] == random_number) then print_calculations(these_numbers) else false end
-	  	sleep 1.2
-		end
-	end
+  NUMBER_SET_AT.times do |integer_1|
+    sum = {}
+      NUMBER_SET_AT.times do |integer_2|
+      these_numbers = [integer_1, integer_2]
+      sum[:calculated] = these_numbers[0] + these_numbers[1]
+      if (sum[:calculated] == random_number) then print_calculations(these_numbers) else false end
+      sleep 1.2
+    end
+  end
 end
 
 find_number()
