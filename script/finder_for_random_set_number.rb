4a5ab71ce70573fd058d871b5ed322c40879d285
kaivongcom <234134+kaivongcom@users.noreply.github.com>
2025-09-08 10:10:20 +0100

    Update adds `Enumerator.sum`

--- script/finder_for_random_set_number.rb
+++ script/finder_for_random_set_number.rb
@@ -1,9 +1,9 @@
-RNDM_NO = '13'
+RNDM_NO = 13
 NUMBER_SET_AT = 21
 LN_BREAK = "\n"
 
 def print_calculations(numbers)
-  print "%s + %s" %numbers
+  print "%s + %s" %[numbers[:one], numbers[:two]]
   print LN_BREAK + "ok!"  + LN_BREAK
 end
 
@@ -11,10 +11,12 @@ def find_number(random_number=RNDM_NO)
   NUMBER_SET_AT.times do |integer_1|
     sum = {}
       NUMBER_SET_AT.times do |integer_2|
-      these_numbers = [integer_1, integer_2]
-      sum[:calculated] = these_numbers[0] + these_numbers[1]
+      these_numbers = {'one': integer_1, 'two':  integer_2}
+			sum[:calculated] = these_numbers.sum { |key, value| value }
       if (sum[:calculated] == random_number) then print_calculations(these_numbers) else false end
       sleep 1.2 # number as float, 1-decimal, of seconds
     end
   end
 end
+
+find_number()
