f342d0d876f9f0eea421e9835786cfae6a263181
kaivongcom <234134+kaivongcom@users.noreply.github.com>
2025-09-14 14:34:49 +0100

    edit script for Array sum

--- script/finder_for_random_set_number.rb
+++ script/finder_for_random_set_number.rb
@@ -5,7 +5,7 @@ LN_BREAK = "\n"
 def those_calculations(numbers)
   if numbers[:calculated] == numbers[:number]
     print (LN_BREAK * 2) + "ok!"  + (LN_BREAK)
-    print "%s + %s" %[numbers[:one], numbers[:two]]
+    print "%s + %s" %[numbers[:sum_rdy][0], numbers[:sum_rdy][1]]
   else
     false 
   end
@@ -15,8 +15,8 @@ end
 def find_number(random_number=RNDM_NO, seeker_number=NUMBER_SET_AT)
   seeker_number.times do |integer_1|
     seeker_number.times do |integer_2|
-      set_hash = {'one': integer_1, 'two':  integer_2 }
-      set_hash[:calculated] = set_hash.sum { |k, v| v }
+      set_hash = {'sum_rdy': [integer_1, integer_2]}
+      set_hash[:calculated] = set_hash[:sum_rdy].sum
       set_hash[:number] = random_number
       those_calculations(set_hash)
     end
